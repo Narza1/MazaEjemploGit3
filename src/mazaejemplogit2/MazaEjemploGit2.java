998d5e0ca5589b4cf97293516e0152dd3788bf53
Narza <Narza@DESKTOP-N39VQMD>
2022-05-03 16:45:08 +0200

    versión 1

---
+++ src/mazaejemplogit2/MazaEjemploGit2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mazaejemplogit2;
+
+/**
+ *
+ * @author Narza
+ */
+public class MazaEjemploGit2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Adrian Maza");
+        System.out.println("Version 1");
+    }
+    
+}
